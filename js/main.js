15bba52505f5c0ae14c02bc678dc07c4c50d8683
Austin Mallar <austinmallar@gmail.com>
2019-09-06 09:28:56 -0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+import { TweenMax, Power2, Power1 } from "gsap";
+
+const toggle = document.querySelector(".toggle");
+const navList = document.querySelector(".nav-list");
+
+const entrance = () => {
+  TweenMax.from(".logo", 1, {
+    y: -30,
+    opacity: 0,
+    ease: Power1.easeInOut
+  });
+
+  TweenMax.staggerFrom(
+    ".nav-item",
+    1,
+    {
+      y: -30,
+      opacity: 0,
+      delay: 0.3,
+      ease: Power2.easeIn
+    },
+    0.2
+  );
+
+  TweenMax.staggerFrom(
+    ".social-icons i",
+    1,
+    {
+      y: 30,
+      opacity: 0,
+      delay: 1,
+      ease: Power2.easeIn
+    },
+    0.2
+  );
+};
+
+toggle.addEventListener("click", () => {
+  if (navList.classList.contains("open")) {
+    navList.classList.remove("open");
+  } else {
+    navList.classList.add("open");
+
+    TweenMax.staggerFrom(
+      ".nav-item",
+      1,
+      {
+        y: -30,
+        opacity: 0,
+        delay: 0.3,
+        ease: Power2.easeIn
+      },
+      0.2
+    );
+  }
+});
+
+entrance();
