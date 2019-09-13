d1f7fe581b2170a7a95c20822366df1d285ca323
Austin Mallar <austinmallar@gmail.com>
2019-09-13 12:37:56 -0700

    added entrance anim

--- js/main.js
+++ js/main.js
@@ -1,61 +1,67 @@
-import { TweenMax, Power2, Power1, TimelineMax } from "gsap";
+import { TimelineMax, Expo } from "gsap";
 
 const toggle = document.querySelector(".toggle");
-const navList = document.querySelector(".nav-list");
+const navLinks = Array.from(document.querySelectorAll(".nav-item"));
+const menu = document.querySelector(".nav-list");
+let menuOpen = false;
+var t1 = new TimelineMax({ paused: true });
+var t2 = new TimelineMax();
 
-const entrance = () => {
-  TweenMax.from(".logo", 1, {
-    y: -30,
-    opacity: 0,
-    ease: Power1.easeInOut
-  });
-
-  TweenMax.staggerFrom(
-    ".nav-item",
-    1,
-    {
-      y: -30,
-      opacity: 0,
-      delay: 1,
-      ease: Power2.easeIn
-    },
-    0.2
-  );
-
-  TweenMax.staggerFrom(
-    ".social-icons i",
-    1,
-    {
-      y: 30,
-      opacity: 0,
-      delay: 2,
-      ease: Power2.easeIn
-    },
-    0.2
-  );
-};
-
-toggle.addEventListener("click", () => {
-  if (navList.classList.contains("open")) {
-    navList.classList.remove("open");
-    toggle.classList.remove("toggle-on");
-    document.querySelector("body").style.overflow = "auto";
-  } else {
-    navList.classList.add("open");
-    toggle.classList.add("toggle-on");
-    document.querySelector("body").style.overflow = "hidden";
-    TweenMax.staggerFrom(
-      ".nav-item",
-      1,
-      {
-        y: -30,
-        opacity: 0,
-        delay: 0.3,
-        ease: Power2.easeIn
-      },
-      0.2
-    );
-  }
+t2.from(".logo", 0.7, {
+  opacity: 0
 });
 
-entrance();
+t2.from(".nav-list", 0.7, {
+  opacity: 0
+});
+
+t2.from(".toggle", 1, {
+  opacity: 0
+});
+
+t2.staggerFrom(
+  ".social-icons i",
+  1,
+  {
+    opacity: 0,
+    x: -20,
+    ease: Expo.easeInOut
+  },
+  0.3,
+  "-=1"
+);
+
+if (window.innerWidth < 768) {
+  t1.to(".nav-list", 1, {
+    scaleY: 1,
+    ease: Expo.easeIn,
+    delay: 0.5
+  });
+  t1.staggerFrom(
+    ".nav-item",
+    1,
+    { y: -200, opacity: 0, ease: Expo.easeOut },
+    0.3
+  );
+}
+
+// navLinks.forEach(link => {
+//   link.style.transform = "translate(0)";
+//   link.style.opacity = "1";
+// });
+// menu.style.transform = scaleY(0);
+
+toggle.addEventListener("click", () => {
+  if (menuOpen) {
+    toggle.classList.remove("toggle-on");
+    document.querySelector("body").style.overflow = "auto";
+    t1.reverse();
+    menuOpen = false;
+  } else {
+    toggle.classList.add("toggle-on");
+    document.querySelector("body").style.overflow = "hidden";
+    t1.play();
+    menuOpen = true;
+  }
+});
+window.addEventListener("resize");
