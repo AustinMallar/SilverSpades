cbcf97911084eb0245f87735f31952d536284421
Austin Mallar <austinmallar@gmail.com>
2019-09-13 19:38:00 -0700

    hamburger animation using gsap

--- js/main.js
+++ js/main.js
@@ -35,27 +35,46 @@ t2.staggerFrom(
 
 //Timeline for mobile nav menu
 if (window.innerWidth < 768) {
+  t1.staggerTo(
+    ".hamburger",
+    0.3,
+    {
+      width: "0px",
+      ease: Expo.easeIn
+    },
+    0.1
+  );
   t1.to(".nav-list", 1, {
     scaleY: 1,
     ease: Expo.easeIn,
     delay: 0.5
   });
+
   t1.staggerFrom(
     ".nav-item",
     0.5,
     { x: -200, opacity: 0, ease: Expo.easeOut },
     0.3
   );
+
+  t1.to("#ham1", 0.3, {
+    width: "30px",
+    rotation: 45,
+    background: "black"
+  });
+  t1.to("#ham2", 0.3, {
+    width: "30px",
+    rotation: -45,
+    background: "black"
+  });
 }
 
 toggle.addEventListener("click", () => {
   if (menuOpen) {
-    toggle.classList.remove("toggle-on");
     document.querySelector("body").style.overflow = "auto";
     t1.reverse();
     menuOpen = false;
   } else {
-    toggle.classList.add("toggle-on");
     document.querySelector("body").style.overflow = "hidden";
     t1.play();
     menuOpen = true;
